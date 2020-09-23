b07c374c3f067b5b48f79a476bd8150de1169b4c
Sp <41648020+TheoJSP@users.noreply.github.com>
2020-09-23 01:23:05 -0300

    Un par de ajustes antiTrolls

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,6 @@
 //inicializaciones
 console.log("Chat.js ON");
+
 const socket = io();
 
 //Elements
@@ -10,34 +11,57 @@ let output = document.getElementById("output");
 let actions = document.getElementById("actions");
 
 //Manda al server la info del user en un objeto 
-btn.addEventListener("click",()=>{
-    socket.emit("chat:message", {
-        username: username.value,
-        message: message.value
-     });
+btn.addEventListener("click", () => {
+    if (username.value == "" || message.value == "") {
+        Swal.fire('Complete los campos requeridos')
+    } else {
+        socket.emit("chat:message", {
+            username: username.value,
+            message: message.value
+        });
+        //lockea el nombre 
+        message.value = ""
+        var name = document.getElementById("username")
+        name.setAttribute("disabled", "disabled")
+    }
+
 });
 
 
 //Manda al server que el usuario esta escribiendo un MSG
-message.addEventListener("keypress", ()=>{
+message.addEventListener("keypress", () => {
     socket.emit("chat:typing", username.value);
 });
 
 //Imprime la info del usuario con su mensaje al ser enviado 
-socket.on("chat:message", (data)=>{
+socket.on("chat:message", (data) => {
     console.log(data);
-    actions.innerHTML = " "
-    output.innerHTML +=  `
-    <div class="input-group mt-2 text-center ">
-        <div class="input-group-prepend">
-            <span class="input-group-text">${data.username}</span>
+    //si no es el emisor 
+    if (data.username != username.value) {
+        actions.innerHTML = " "
+        output.innerHTML += `
+        <div class="input-group mt-2 text-center ">
+            <div class="input-group-prepend">
+                <span class="input-group-text">${data.username}</span>
+            </div>
+            <input  type="text" class="form-control font-weight-bold" disabled=disabled value="${data.message}">
         </div>
-        <input  type="text" aria-label="First name" class="form-control" disabled=disabled placeholder=" ${data.message}">
-    </div>
     `
+        //si es el emisor
+    } else if (data.username == username.value) {
+        actions.innerHTML = " "
+        output.innerHTML += ` 
+        <div class="input-group mt-2 ">
+        <input type="text" class="form-control font-weight-bold" disabled=disabled value="${data.message}" >
+        <div class="input-group-append">
+          <span class="input-group-text" id="basic-addon2">${data.username}</span>
+        </div>
+      </div> `
+
+    }
 });
 
 //Envia que el usuario X esta escribiendo 
-socket.on("chat:typing", (data)=>{
+socket.on("chat:typing", (data) => {
     actions.innerHTML = `<p><em>${data} Esta escribiendo un mensaje... </em</p>`
 });
